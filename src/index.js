592c66719d6b9e7083a5b28ade8cdd6aaab5ca32
George Barrett <georgeaustinbarrett@gmail.com>
2022-10-11 13:27:56 +0100

    initial git commit api set up

---
+++ src/index.js
@@ -0,0 +1,37 @@
+// importing dependencies 
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+// defining the express app
+const app = express();
+
+// defining an array to work as the database
+const ads = [
+    {title: 'This is Sparta'}
+];
+
+// adding helmet for api security
+app.use(helmet());
+
+// using bodyParser to parse json bodies into js objects
+app.use(bodyParser.json());
+
+// enabling cors for all requests
+app.use(cors());
+
+// adding morgan to log https requests
+app.use(morgan('combined'));
+
+// defining an endpoint to return all ads
+app.get('/george', (req, res) => {
+    res.send(ads);
+});
+
+// starting the server
+app.listen(3001, () => {
+    console.log('listening on port 3001');
+});
+
